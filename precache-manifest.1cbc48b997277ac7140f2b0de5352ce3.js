96e90d0a9acf6b8150bd257f2551b5db91c1798e
Brandon Jackson <brandonljackson23@gmail.com>
2020-10-04 11:40:10 -0600

    Updates

---
+++ precache-manifest.1cbc48b997277ac7140f2b0de5352ce3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec6e3a4b5d822ad5b2934a88a4f27b88",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "306955287541ea559348",
+    "url": "/react-portfolio/static/css/main.5ebc4642.chunk.css"
+  },
+  {
+    "revision": "2e20edfd9536d30ce5c8",
+    "url": "/react-portfolio/static/js/2.561b7ba5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.561b7ba5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "306955287541ea559348",
+    "url": "/react-portfolio/static/js/main.5535124f.chunk.js"
+  },
+  {
+    "revision": "d2973fa5f1f6dd99cb4f",
+    "url": "/react-portfolio/static/js/runtime-main.4785a1c3.js"
+  },
+  {
+    "revision": "6386dd1b8e19d8ff67667649fc6739ff",
+    "url": "/react-portfolio/static/media/budget-tracker.6386dd1b.PNG"
+  },
+  {
+    "revision": "e863ae4337943b79f76b236818c95622",
+    "url": "/react-portfolio/static/media/cover-image.e863ae43.png"
+  },
+  {
+    "revision": "ef0a538701facdd47088bf447b69df13",
+    "url": "/react-portfolio/static/media/daily-scheduler.ef0a5387.PNG"
+  },
+  {
+    "revision": "18f6c6d81d4be340b3d86b03a1b86014",
+    "url": "/react-portfolio/static/media/death-roll.18f6c6d8.PNG"
+  },
+  {
+    "revision": "64d7790df7e10d9ddccfbf1f6fcbe8d1",
+    "url": "/react-portfolio/static/media/js-quiz.64d7790d.PNG"
+  },
+  {
+    "revision": "e9080974ad893ac37f6e35a0438df604",
+    "url": "/react-portfolio/static/media/password-generator.e9080974.PNG"
+  },
+  {
+    "revision": "82aae4397dfa4ce56562b157b6e74907",
+    "url": "/react-portfolio/static/media/tech-blog.82aae439.PNG"
+  },
+  {
+    "revision": "872c284befda815dbc381a8080d9198b",
+    "url": "/react-portfolio/static/media/weather-dashboard.872c284b.PNG"
+  },
+  {
+    "revision": "8f7f9098356bd23a4e7594e9f0e1c525",
+    "url": "/react-portfolio/static/media/what-2-watch.8f7f9098.PNG"
+  }
+]);
